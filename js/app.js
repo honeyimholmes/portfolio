0693b3c107a9674e62293360e9eaf8281fcc5a32
Karolina Romankiewicz <karolina.romankiewicz@gmail.com>
2017-05-24 20:48:11 +0200

    main layout

---
+++ js/app.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    var menuBtn = document.querySelector(".menu-btn");
+    var menuLeft = document.querySelector(".navbar-fixed-left");
+    var menuLeftHidden = document.querySelector(".menu-left-hidden");
+    var menuPortfolio = document.querySelector(".menu-portfolio");
+    var menuPortHidden = document.querySelector(".menu-portfolio-hidden");
+    var socialList = document.querySelector(".social-list");
+    var socialListHidden = document.querySelector(".social-list-hidden");
+    var menuClass = document.querySelector(".menu");
+    var redaktor = document.querySelector(".red");
+    var portfolioRed = document.querySelector(".header-port-1");
+    var portfolioFront = document.querySelector(".header-port-2");
+    var feDev = document.querySelector(".fed");
+    var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+    
+    menuBtn.addEventListener("click", function(e){
+        menuLeft.classList.toggle("navbar-fixed-full");
+        menuBtn.classList.toggle("fa-code");
+        menuLeftHidden.classList.toggle("menu-show");
+        socialList.classList.toggle("social-close");
+        socialListHidden.classList.toggle("social-list-open");
+        menuClass.style.marginTop = "30px";
+    }); 
+    
+    redaktor.addEventListener("mouseover", function(e){
+        portfolioRed.style.opacity = "1";
+    });
+    
+    feDev.addEventListener("mouseover", function(e){
+        portfolioFront.style.opacity = "1";
+    })
+    
+                    
+});
